b0faddc158a6a4eb25ebdfa82579d6e853706eeb
Janik Hotz <snangles.void@gmail.com>
2020-03-13 20:07:41 +0100

    setup

--- backend/dist/admin/secure/js/23.e2a2d332c5c8d741abc2.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[23],{832:function(e,i,s){"use strict";s.r(i);var t=s(1);i.default=({data:e})=>e?Object(t.jsx)("div",{css:{alignItems:"center",display:"flex",height:24,lineHeight:0,width:24}},Object(t.jsx)("img",{alt:e.filename,css:{maxHeight:"100%",maxWidth:"100%"},src:e.publicUrlTransformed})):null}}]);
--- backend/dist/admin/secure/js/27.e2a2d332c5c8d741abc2.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[27],{820:function(t,a,n){"use strict";n.r(a);var o=n(249);a.default=t=>{if(!t.data)return null;const a=t.field.config.format;return a?Object(o.format)(t.data,a):t.data}}}]);
